709ff205e2a2f3f26315ddf9caaa01c531a3dbf0
ultimateshop ru <tysonalbarran@cox.net>
2025-11-04 01:20:44 +0100

    Translation update (en)

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,14 +5,35 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author ultimateshop ru <tysonalbarran@cox.net>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  * @author Esther Brunner <wikidesign@gmail.com>
  */
-$lang['page']                  = 'Page';
-$lang['date']                  = 'Date';
-$lang['user']                  = 'User';
-$lang['desc']                  = 'Description';
-$lang['diff']                  = 'Differences';
-$lang['summary']               = 'Edit summary';
-$lang['diff_title']            = 'Show differences to current revisions';
-$lang['diff_alt']              = 'Show differences to current revisions';
+$lang['page']                  = 'Excellent post. I was checking continuously this blog and 
+I\'m impressed! Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['date']                  = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+
+Extremely useful info specially the last part :
+) I care for such information a lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['user']                  = 'ultimateshop ru';
+$lang['desc']                  = 'Excellent post. I was checking continuously this blog 
+and I\'m impressed! Extremely useful info specially the last part :) I care for such information a lot.
+
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['diff']                  = 'Excellent post. I was checking continuously 
+this blog and I\'m impressed! Extremely useful info specially the last 
+part :) I care for such information a lot. I was looking for this certain information for a long 
+time. Thank you and best of luck.';
+$lang['summary']               = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a 
+lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['diff_title']            = 'I am the new one';
+$lang['diff_alt']              = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -5,34 +5,106 @@
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author ultimateshop ru <tysonalbarran@cox.net>
  * @author Esther Brunner <wikidesign@gmail.com>
  */
-$lang['style']                 = 'Style of pagelist';
-$lang['style_o_default']       = 'default';
-$lang['style_o_table']         = 'table';
-$lang['style_o_list']          = 'table/list';
-$lang['style_o_simple']        = 'simple list';
-$lang['showheader']            = 'show heading line';
-$lang['showdate']              = 'show date column';
-$lang['showdate_o_0']          = 'hide';
-$lang['showdate_o_1']          = 'creation date';
-$lang['showdate_o_2']          = 'modification date';
-$lang['showuser']              = 'show user column';
-$lang['showuser_o_0']          = 'hide';
-$lang['showuser_o_1']          = 'creator';
-$lang['showuser_o_2']          = 'contributors';
-$lang['showuser_o_3']          = 'creator (in "showuseras" format)';
-$lang['showuser_o_4']          = 'contributors (in "showuseras" format)';
-$lang['showdesc']              = 'show description column';
-$lang['showdesc_o_0']          = 'hide';
-$lang['showdesc_o_160']        = 'max. 160 chars';
-$lang['showdesc_o_500']        = 'max. 500 chars';
-$lang['showcomments']          = 'show comments column (Discussion Plugin needed)';
-$lang['showlinkbacks']         = 'show linkbacks column (Linkback Plugin needed)';
-$lang['showtags']              = 'show tags column (Tag Plugin needed)';
-$lang['showfirsthl']           = 'show the first headline instead of the page name';
-$lang['showimage']             = 'show image column (Pageimage Plugin needed)';
-$lang['showdiff']              = 'show link to the diff page';
-$lang['sort']                  = 'sort pages by pagename';
-$lang['rsort']                 = 'reverse sort pages by pagename';
-$lang['sortby']                = 'sort the page by the given item e.g. <code>id</code>, <code>pagename</code>, <code>ns</code>, <code>title</code>, <code>date</code>, <code>desc</code>, <code>user</code> etc or columns from plugins';
+$lang['style']                 = 'Excellent post. I was checking continuously this blog and I\'m 
+impressed! Extremely useful info specially the last part :) I care for 
+such information a lot. I was looking for this certain information for a long 
+time. Thank you and best of luck.';
+$lang['style_o_default']       = 'Excellent post. I was checking continuously this blog 
+and I\'m impressed! Extremely useful info specially the last part :) I care for such 
+information a lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['style_o_table']         = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['style_o_list']          = 'Excellent post. I was checking continuously 
+this blog and I\'m impressed! Extremely useful info 
+specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['style_o_simple']        = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a 
+lot. I was looking for this certain information for a 
+long time. Thank you and best of luck.';
+$lang['showheader']            = 'I am the new one';
+$lang['showdate']              = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long 
+time. Thank you and best of luck.';
+$lang['showdate_o_0']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part 
+:) I care for such information a lot. I was looking for 
+this certain information for a long time. Thank you and best of 
+luck.';
+$lang['showdate_o_1']          = 'Excellent post. I was checking continuously this blog and 
+I\'m impressed! Extremely useful info specially the last part :) I care 
+for such information a lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showdate_o_2']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showuser']              = 'Tyson';
+$lang['showuser_o_0']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showuser_o_1']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showuser_o_2']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+
+I was looking for this certain information for a long time.
+
+Thank you and best of luck.';
+$lang['showuser_o_3']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :
+) I care for such information a lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showuser_o_4']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showdesc']              = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last 
+part :) I care for such information a lot. I was looking for this certain information for a 
+long time. Thank you and best of luck.';
+$lang['showdesc_o_0']          = 'Excellent post. I was checking continuously this blog and 
+I\'m impressed! Extremely useful info specially the last part :) I 
+care for such information a lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showdesc_o_160']        = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showdesc_o_500']        = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :
+) I care for such information a lot. I was looking for this certain information for a long time.
+Thank you and best of luck.';
+$lang['showcomments']          = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care 
+for such information a lot. I was looking for this certain information for 
+a long time. Thank you and best of luck.';
+$lang['showlinkbacks']         = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+
+Extremely useful info specially the last part :
+) I care for such information a lot. I was looking for this certain information for a 
+long time. Thank you and best of luck.';
+$lang['showtags']              = '28';
+$lang['showfirsthl']           = 'Tyson';
+$lang['showimage']             = 'Excellent post. I was checking continuously this blog and I\'m impressed!
+Extremely useful info specially the last part :) I care for such information a lot.
+
+I was looking for this certain information for a long time.
+
+Thank you and best of luck.';
+$lang['showdiff']              = 'https://ultimateshop.biz/';
+$lang['sort']                  = 'Rotterdam';
+$lang['rsort']                 = 'Rotterdam';
+$lang['sortby']                = 'Rotterdam';
