41c2816ee579332433fca6be3354a28ae7d497d4
Marek Adamski <fevbew@wp.pl>
2026-04-19 01:50:45 +0200

    Translation update (pl)

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author Michał <kamykowsky@gmail.com>
  */
 $lang['style']                 = 'Styl listy stron';
@@ -19,9 +20,18 @@ $lang['showuser']              = 'pokaż kolumnę użytkownika';
 $lang['showuser_o_0']          = 'ukryj';
 $lang['showuser_o_1']          = 'twórca';
 $lang['showuser_o_2']          = 'współtwórcy';
+$lang['showuser_o_3']          = 'twórca (w formacie „showuseras”)';
+$lang['showuser_o_4']          = 'współautorzy (w formacie „showuseras”)';
 $lang['showdesc']              = 'pokaż kolumnę opisu';
 $lang['showdesc_o_0']          = 'ukryj';
 $lang['showdesc_o_160']        = 'max. 160 znaków';
 $lang['showdesc_o_500']        = 'max. 500 znaków';
 $lang['showcomments']          = 'pokaż kolumnę komentarzy (wymagany Discussion Plugin)';
+$lang['showlinkbacks']         = 'pokaż kolumnę linków zwrotnych (wymagana wtyczka Linkback)';
 $lang['showtags']              = 'Pokaż kolumnę tagów (wymagany Tag Plugin)';
+$lang['showfirsthl']           = 'pokaż pierwszy nagłówek zamiast nazwy strony';
+$lang['showimage']             = 'pokaż kolumnę z obrazkami (potrzebna wtyczka Pageimage)';
+$lang['showdiff']              = 'pokaż link do strony diff';
+$lang['sort']                  = 'sortuj strony według nazwy strony';
+$lang['rsort']                 = 'odwrotnie sortuj strony według nazwy strony';
+$lang['sortby']                = 'sortuj stronę według podanego elementu, np. <code>id</code>, <code>pagename</code>, <code>ns</code>, <code>title</code>, <code>date</code>, <code>desc</code>, <code>user</code> itp. lub kolumn z wtyczek';
